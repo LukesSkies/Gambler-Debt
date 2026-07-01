b624a29f2389991cadd268ef1c06c13779e1a5b3
LukesSkies <luke.webb02@outlook.com>
2026-07-01 16:56:07 +0100

    Bar Blockout

--- Assets/Scripts/Intro/IntroDialogueData.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Intro/Dialogue Sequence")]
-public class IntroDialogueData : ScriptableObject
-{
-    [TextArea(3, 6)]
-    public string[] lines;
-}
